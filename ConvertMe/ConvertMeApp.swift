2a11f0fad3c4c56e01a897d480ec65f3c096a55d
akatzfey <katzfeyalex@gmail.com>
2022-02-18 12:47:31 -0800

    Initial Commit

---
+++ ConvertMe/ConvertMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeApp.swift
+//  ConvertMe
+//
+//  Created by Alexander Katzfey on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
